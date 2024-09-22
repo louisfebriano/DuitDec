a59e174af3394dc367e06d1edcf158300fa7b77d
HengkyLaurencio <liu.hengky.laurencio@gmail.com>
2024-09-23 00:01:38 +0700

    fix overflow and add feautures section

--- assets/js/landingpage.js
+++ assets/js/landingpage.js
@@ -1,31 +1,52 @@
 window.addEventListener('load', () => {
     const splashScreen = document.getElementById('splash-screen');
     const navbar = document.getElementById('navbar');
-    const logo = document.querySelector('.logo');
     const logoNavbar = document.querySelector('.logo-navbar');
     const navbarListItems = document.querySelectorAll('.navbar-list li');
+    const cardContainer = document.getElementById('card-container');
+    const cardLeft = document.querySelector('.card-left');
+    const cardRight = document.querySelector('.card-right');
+    const centerCard = document.getElementById('center-card');
+    const body = document.body; // Ambil elemen body
 
-    // Step 1: Initial logo animation (already handled by CSS)
-    
+    // Tambahkan kelas no-scroll pada body
+    body.classList.add('no-scroll');
+
     setTimeout(() => {
-        // Step 2: Hide splash screen and show navbar
         splashScreen.style.opacity = '0';
-        splashScreen.style.transition = 'opacity 1s ease-out';
+        splashScreen.style.transition = 'opacity 0.5s ease-out';
         navbar.style.transform = 'translateY(0)';
         navbar.style.opacity = '1';
-        
-        setTimeout(() => {
-            // Remove splash screen from the DOM after transition
-            splashScreen.style.display = 'none';
 
-            // Move logo to navbar
+        setTimeout(() => {
+            splashScreen.style.display = 'none';
             logoNavbar.style.opacity = '1';
-            
-            // Step 3: Animate navbar items
+
+            // Hapus kelas no-scroll dari body setelah splash screen dihapus
+            body.classList.remove('no-scroll');
+
             navbarListItems.forEach((item, index) => {
                 item.style.opacity = '1';
                 item.style.transform = 'translateY(0)';
+                item.style.transition = `opacity 0.5s ease ${0.1 * index}s`;
             });
-        }, 1000);
-    }, 1000);
+
+            setTimeout(() => {
+                centerCard.style.opacity = '1';
+                centerCard.style.transform = 'translateY(0)';
+                cardContainer.style.opacity = '1';
+                cardContainer.style.transform = 'translateY(0)';
+                cardLeft.style.opacity = '1';
+                cardLeft.style.transform = 'translateY(0)';
+                cardRight.style.opacity = '1';
+                cardRight.style.transform = 'translateY(0)';
+            }, 300);
+        }, 500);
+    }, 500);
+
+    setTimeout(() => {
+        const featuresSection = document.getElementById('features-section');
+        featuresSection.style.opacity = '1';
+        featuresSection.style.transform = 'translateY(0)';
+    }, 1500);
 });
