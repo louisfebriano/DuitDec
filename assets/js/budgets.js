65ee83044e3d34c8d916622073dabf2131c22615
HengkyLaurencio <liu.hengky.laurencio@gmail.com>
2024-10-10 10:02:32 +0700

    add new budgets button

--- assets/js/budgets.js
+++ assets/js/budgets.js
@@ -1,3 +1,16 @@
+const modal = document.getElementById('modal');
+const addButton = document.getElementById('addButton');
+const cancelButton = document.getElementById('cancelButton');
+
+
+addButton.addEventListener('click', () => {
+    modal.style.display = 'block';
+});
+
+cancelButton.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
 
 
 $(document).ready(function() {
